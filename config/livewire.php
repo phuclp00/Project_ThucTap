2c496aa77658a774f779b9dcc51954a10ccd3809
phuclp00 <62292782+phuclp00@users.noreply.github.com>
2021-05-05 19:55:05 +0700

    update

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => null,
+    'asset_url' => "http://laptrinhweb-bookstore.tk/public",
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/', function () {
         return view('dashboard');
